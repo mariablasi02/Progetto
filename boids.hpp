84cd3fa2be0be8a071fc6fab7e00e66ed5ea881c
Camilla Berti <camillaberti18@gmail.com>
2022-08-10 13:24:18 +0200

    Adjusted tests

--- boids.hpp
+++ boids.hpp
@@ -12,6 +12,7 @@ struct BoidState {
   double y{};
   double v_x{};
   double v_y{};
+  //BoidState(double x, double y, double vx, double vy) : x{x}, y{y}, v_x{vx}, v_y{vy} {};
   BoidState& operator+=(BoidState const& other) {
     x += other.x;
     y += other.y;
@@ -21,11 +22,11 @@ struct BoidState {
   }
 };
 
-BoidState operator==(BoidState const& b1, BoidState const& b2) {
-  return {b1.x == b2.x, b1.y == b2.y, b1.v_x == b2.v_x, b1.v_y == b2.v_y};
+bool operator==(BoidState const& b1, BoidState const& b2) {
+  return {b1.x == b2.x && b1.y == b2.y && b1.v_x == b2.v_x && b1.v_y == b2.v_y};
 }
 
-BoidState operator!=(BoidState const& b1, BoidState const& b2) {
+bool operator!=(BoidState const& b1, BoidState const& b2) {
   return {b1.x != b2.x || b1.y != b2.y || b1.v_x != b2.v_x ||
           b1.v_y != b2.v_y};  // così basta che solo una delle quattro
                               // condizioni non sia vera
@@ -50,10 +51,8 @@ BoidState operator/(BoidState const& b1, BoidState const& b2) {
 }
 
 double norm(BoidState const& b1, BoidState const& b2) {
-  auto result =
-      std::abs((b1.x - b2.x) * (b1.x - b2.x) + (b1.y - b2.y) * (b1.y - b2.y));
-  assert(result > 0);  // non so se abbia senso ora mantenere questo assert
-                       // visto che ho messo ||
+  auto result = (b1.x - b2.x) * (b1.x - b2.x) + (b1.y - b2.y) * (b1.y - b2.y);
+  assert(!(result < 0)); 
   return std::sqrt(result);
 }
 
@@ -75,9 +74,7 @@ class SeparationRule {
   SeparationRule(int const n, double const s) : n_{n}, separation_const_{s} {}
 
   VelocityComponents operator()(BoidState const& b1,
-                                BoidState const& b2) const {
-    return;
-  }
+                                BoidState const& b2) const; //only declaration
 };
 
 class AllignmentRule {};
--- boids.test.cpp
+++ boids.test.cpp
@@ -3,81 +3,99 @@
 #include "boids.hpp"
 #include "doctest.h"
 
-SUBCASE("Check norm2 <= 0") {
+TEST_CASE("Testing norm function"){
   BoidState b1{2., 3., 5., 5.};
   BoidState b2{2., 3., 4., 4.};
-  CHECK(((b1.x - b2.x) * (b1.x - b2.x) + (b1.y - b2.y) * (b1.y - b2.y)) == 0.);
+  CHECK(norm(b1, b2) == 0.);
 }
 
 TEST_CASE("Testing operators") {
   SUBCASE("Check operator == on identical boids") {
     BoidState b1{2., 3., 4., 0.};
     BoidState b2{2., 3., 4., 0.};
-    CHECK(b1 == b2);
+    CHECK((b1 == b2) == true);
   }
 
   SUBCASE("Check operator == on different boids") {
     BoidState b1{2., 2., 4., 0.};
     BoidState b2{2., 3., 4., 0.};
-    CHECK_FALSE(b1 == b2);
+    CHECK((b1 == b2) == false);
   }
 
   SUBCASE("Check operator != on different boids") {
     BoidState b1{2., 5., 3., 1.};
     BoidState b2{1., 3., 4., 0.};
-    CHECK(b1 != b2);
+    CHECK((b1 != b2) == true);
   }
 
   SUBCASE("Check operator != on identical boids") {
     BoidState b1{2., 3., 4., 0.};
     BoidState b2{2., 3., 4., 0.};
-    CHECK_FALSE(b1 != b2);
+    CHECK((b1 != b2) == false);
   }
+
   SUBCASE("Check addition with two points") {
     BoidState b1{1., 2., 3., 4.};
     BoidState b2{4., 3., 2., 1.};
-    CHECK(((b1 + b2) ==
-           {5., 5., 5., 5.}));  // mi dà errore sui check non capisco perchè
+    CHECK((b1 + b2).x == 5.);
+    CHECK((b1 + b2).y == 5.);
+    CHECK((b1 + b2).v_x == 5.);
+    CHECK((b1 + b2).v_y == 5.);  // mi dà errore sui check non capisco perchè
   }
   SUBCASE("Check addition with three points") {
     BoidState b1{1., 2., 3., 4.};
     BoidState b2{4., 3., 2., 1.};
     BoidState b3{5., 9., 2., 6.};
-    CHECK(((b1 + b2) == {10., 14., 7., 11.}));
+    CHECK((b1 + b2 + b3).x == 10.);
+    CHECK((b1 + b2 + b3).y == 14.);
+    CHECK((b1 + b2 + b3).v_x == 7.);
+    CHECK((b1 + b2 + b3).v_y == 11.);
   }
   SUBCASE("Check addition with negative values") {
     BoidState b1{-1., 2., 3., 4.};
     BoidState b2{4., -3., 2., -1.};
-    CHECK((b1 + b2) == {3., -1., 5., 3.});
+    CHECK((b1 + b2).x == 3.);
+    CHECK((b1 + b2).y == -1.);
+    CHECK((b1 + b2).v_x == 5.);
+    CHECK((b1 + b2).v_y == 3.);
   }
 
   SUBCASE("Check addition with zero") {
     BoidState b1{1., 2., 3., 4.};
     BoidState b2{0., 0., 0., 0.};
-    CHECK(((b1 + b2) == {1., 2., 3., 4.}));
+    CHECK(((b1 + b2) == b1));
   }
 
   SUBCASE("Check difference with zero") {
     BoidState b1{1., 2., 3., 4.};
     BoidState b2{0., 0., 0., 0.};
-    CHECK(((b1 - b2) == {1., 2., 3., 4.}));
+    CHECK(((b1 - b2) == b1));
   }
   SUBCASE("Check difference with two points ") {
     BoidState b1{1., 2., 3., 4.};
     BoidState b2{4., 3., 2., 1.};
-    CHECK(((b1 - b2) == {-3., -1., 1., 3.}));
+    CHECK((b1 - b2).x == -3.);
+    CHECK((b1 - b2).y == -1.);
+    CHECK((b1 - b2).v_x == 1.);
+    CHECK((b1 - b2).v_y == 3.);
   }
   SUBCASE("Check difference with negative numbers") {
     BoidState b1{1., 2., 3., 4.};
     BoidState b2{-1., -3., -2., -4.};
-    CHECK(((b1 - b2) == {2., 5., 5., 8.}));
+    CHECK((b1 - b2).x == 2.);
+    CHECK((b1 - b2).y == 5.);
+    CHECK((b1 - b2).v_x == 5.);
+    CHECK((b1 - b2).v_y == 8.);
   }
   SUBCASE("Check difference which is zero") {
     BoidState b1{1., 2., 3., 4.};
     BoidState b2{1., 2., 3., 4.};
-    CHECK(((b1 - b2) == {0., 0., 0., 0.}));
+    CHECK((b1 - b2).x == 0.);
+    CHECK((b1 - b2).y == 0.);
+    CHECK((b1 - b2).v_x == 0.);
+    CHECK((b1 - b2).v_y == 0.);
   }
-  SUBCASE("Check denominator == 0") {
+  /*SUBCASE("Check denominator == 0") {
     BoidState b1{2., 0., 5., 5.};
     BoidState b2{2., 3., 4., 0.};
     CHECK(operator/(b1, b2));
@@ -86,5 +104,5 @@ TEST_CASE("Testing operators") {
     BoidState b1{2., 0., 5., 5.};
     BoidState b2{2., 3., 4., 0.};
     CHECK((b1 * b2) == {4., 0., 20., 0});
-  }
+  }*/ //commentati perché non credo ci serviranno
 }
