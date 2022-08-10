5dffb86c406e5af329889fc4f618aea8473caacb
Ilaria Antonellini <antonellinilaria@gmail.com>
2022-08-10 20:08:36 +0200

    Add function object in CohesionRule

--- boids.hpp
+++ boids.hpp
@@ -4,6 +4,8 @@
 #include <algorithm>
 #include <cassert>
 #include <cmath>
+#include <iterator>
+#include <numeric>
 #include <stdexcept>
 #include <vector>
 
@@ -14,6 +16,8 @@ struct BoidState {
   double y{};
   double v_x{};
   double v_y{};
+  // BoidState(double x, double y, double vx, double vy) : x{x}, y{y}, v_x{vx},
+  // v_y{vy} {};
   BoidState& operator+=(BoidState const& other) {
     x += other.x;
     y += other.y;
@@ -23,11 +27,11 @@ struct BoidState {
   }
 };
 
-BoidState operator==(BoidState const& b1, BoidState const& b2) {
-  return {b1.x == b2.x, b1.y == b2.y, b1.v_x == b2.v_x, b1.v_y == b2.v_y};
+bool operator==(BoidState const& b1, BoidState const& b2) {
+  return {b1.x == b2.x && b1.y == b2.y && b1.v_x == b2.v_x && b1.v_y == b2.v_y};
 }
 
-BoidState operator!=(BoidState const& b1, BoidState const& b2) {
+bool operator!=(BoidState const& b1, BoidState const& b2) {
   return {b1.x != b2.x || b1.y != b2.y || b1.v_x != b2.v_x ||
           b1.v_y != b2.v_y};  // così basta che solo una delle quattro
                               // condizioni non sia vera
@@ -41,6 +45,10 @@ BoidState operator+(BoidState const& b1, BoidState const& b2) {
 BoidState operator-(BoidState const& b1, BoidState const& b2) {
   return {b1.x - b2.x, b1.y - b2.y, b1.v_x - b2.v_x, b1.v_y - b2.v_y};
 }
+
+BoidState operator*(BoidState const& b1, double const d) {
+  return {b1.x * d, b1.y * d, b1.v_x * d, b1.v_y * d};
+}
 BoidState operator*(BoidState const& b1, BoidState const& b2) {
   return {b1.x * b2.x, b1.y * b2.y, b1.v_x * b2.v_x, b1.v_y * b2.v_y};
 }
@@ -52,10 +60,8 @@ BoidState operator/(BoidState const& b1, BoidState const& b2) {
 }
 
 double norm(BoidState const& b1, BoidState const& b2) {
-  auto result =
-      std::abs((b1.x - b2.x) * (b1.x - b2.x) + (b1.y - b2.y) * (b1.y - b2.y));
-  assert(result > 0);  // non so se abbia senso ora mantenere questo assert
-                       // visto che ho messo ||
+  auto result = (b1.x - b2.x) * (b1.x - b2.x) + (b1.y - b2.y) * (b1.y - b2.y);
+  assert(!(result < 0));
   return std::sqrt(result);
 }
 
@@ -65,6 +71,24 @@ struct VelocityComponents {
   double vel_x;
   double vel_y;
 };
+
+VelocityComponents operator+(VelocityComponents const& c1,
+                             VelocityComponents const& c2) {
+  return {c1.vel_x + c2.vel_x, c2.vel_y + c2.vel_y};
+}
+
+VelocityComponents operator-(VelocityComponents const& c1,
+                             VelocityComponents const& c2) {
+  return {c1.vel_x - c2.vel_x, c2.vel_y - c2.vel_y};
+}
+
+VelocityComponents operator*(VelocityComponents const& c1, double const d) {
+  return {c1.vel_x * d, c1.vel_y * d};
+}
+
+bool operator==(VelocityComponents const& c1, VelocityComponents const& c2) {
+  return {c1.vel_x == c2.vel_x && c1.vel_y == c2.vel_y};
+}
 
 // idea : si potrebbero implementare le classi delle regole già con dei vettori
 // che restituiscono le regole -> dopo mando audio
@@ -82,9 +106,7 @@ class SeparationRule {
       : n_{n}, separation_const_{s}, distance_s_{ds} {}
 
   VelocityComponents operator()(BoidState const& b1,
-                                BoidState const& b2) const {
-    return {};
-  }
+                                BoidState const& b2) const;  // only declaration
 };
 
 class AllignmentRule {
@@ -100,6 +122,20 @@ class AllignmentRule {
   }
 };
 
+VelocityComponents COM(int const n, std::vector<BoidState> b) {
+  auto cboid_it = b.begin();
+  auto cboid_it_next = std::next(b.begin());
+  // auto cboid_last = std::prev(b_.end());
+  if (n > 1) {
+    BoidState sum =
+        std::accumulate(cboid_it_next, b.end(), BoidState{0., 0., 0., 0.});
+    sum = sum * (1 / (n - 1));
+    return {sum.x, sum.y};
+  } else {
+    throw std::runtime_error{"Error: must be n>1"};
+  }
+}
+
 class CohesionRule {
   int const n_;
   double const cohesion_const_;
@@ -107,11 +143,12 @@ class CohesionRule {
  public:
   CohesionRule(int const n, double const c) : n_{n}, cohesion_const_{c} {}
 
-  VelocityComponents COM(int const n_, BoidState const& b1) { return {}; }
-
-  VelocityComponents operator()(BoidState const& b1,
-                                BoidState const& b2) const {
-    return {};
+  VelocityComponents operator()(std::vector<BoidState> cboids) const {
+    auto bi = *cboids.begin();
+    VelocityComponents position_of_c = COM(n_, cboids);
+    BoidState com{position_of_c.vel_x, position_of_c.vel_y, 0., 0.};
+    BoidState result = (com - bi) * cohesion_const_;
+    return {result.x, result.y};
   }
 };
 
@@ -154,9 +191,7 @@ class Boids {
 
   void evolution(double const delta_t) {}
 
-  std::vector<BoidState> const& state() const {
-    return boids_;
-  }  // non capisco perchè dia errore qui
+  std::vector<BoidState> const& state() const;
 };
 
 #endif
--- boids.test.cpp
+++ boids.test.cpp
@@ -3,81 +3,101 @@
 #include "boids.hpp"
 #include "doctest.h"
 
+TEST_CASE("Testing norm function") {
+  BoidState b1{2., 3., 5., 5.};
+  BoidState b2{2., 3., 4., 4.};
+  BoidState b3{5., 6., 0., 0.};
+  CHECK(norm(b1, b3) == doctest::Approx(4.24).epsilon(0.01));
+  CHECK(norm(b1, b2) == 0.);
+}
+
 TEST_CASE("Testing operators") {
-  SUBCASE("Check norm2 <= 0") {
-    BoidState b1{2., 3., 5., 5.};
-    BoidState b2{2., 3., 4., 4.};
-    CHECK(((b1.x - b2.x) * (b1.x - b2.x) + (b1.y - b2.y) * (b1.y - b2.y)) ==
-          0.);
-  }
   SUBCASE("Check operator == on identical boids") {
     BoidState b1{2., 3., 4., 0.};
     BoidState b2{2., 3., 4., 0.};
-    CHECK(b1 == b2);
+    CHECK((b1 == b2) == true);
   }
 
   SUBCASE("Check operator == on different boids") {
     BoidState b1{2., 2., 4., 0.};
     BoidState b2{2., 3., 4., 0.};
-    CHECK_FALSE(b1 == b2);
+    CHECK((b1 == b2) == false);
   }
 
   SUBCASE("Check operator != on different boids") {
     BoidState b1{2., 5., 3., 1.};
     BoidState b2{1., 3., 4., 0.};
-    CHECK(b1 != b2);
+    CHECK((b1 != b2) == true);
   }
 
   SUBCASE("Check operator != on identical boids") {
     BoidState b1{2., 3., 4., 0.};
     BoidState b2{2., 3., 4., 0.};
-    CHECK_FALSE(b1 != b2);
+    CHECK((b1 != b2) == false);
   }
+
   SUBCASE("Check addition with two points") {
     BoidState b1{1., 2., 3., 4.};
     BoidState b2{4., 3., 2., 1.};
-    CHECK(((b1 + b2) ==
-           {5., 5., 5., 5.}));  // mi dà errore sui check non capisco perchè
+    CHECK((b1 + b2).x == 5.);
+    CHECK((b1 + b2).y == 5.);
+    CHECK((b1 + b2).v_x == 5.);
+    CHECK((b1 + b2).v_y == 5.);  // mi dà errore sui check non capisco perchè
   }
   SUBCASE("Check addition with three points") {
     BoidState b1{1., 2., 3., 4.};
     BoidState b2{4., 3., 2., 1.};
     BoidState b3{5., 9., 2., 6.};
-    CHECK(((b1 + b2) == {10., 14., 7., 11.}));
+    CHECK((b1 + b2 + b3).x == 10.);
+    CHECK((b1 + b2 + b3).y == 14.);
+    CHECK((b1 + b2 + b3).v_x == 7.);
+    CHECK((b1 + b2 + b3).v_y == 11.);
   }
   SUBCASE("Check addition with negative values") {
     BoidState b1{-1., 2., 3., 4.};
     BoidState b2{4., -3., 2., -1.};
-    CHECK((b1 + b2) == {3., -1., 5., 3.});
+    CHECK((b1 + b2).x == 3.);
+    CHECK((b1 + b2).y == -1.);
+    CHECK((b1 + b2).v_x == 5.);
+    CHECK((b1 + b2).v_y == 3.);
   }
 
   SUBCASE("Check addition with zero") {
     BoidState b1{1., 2., 3., 4.};
     BoidState b2{0., 0., 0., 0.};
-    CHECK(((b1 + b2) == {1., 2., 3., 4.}));
+    CHECK(((b1 + b2) == b1));
   }
 
   SUBCASE("Check difference with zero") {
     BoidState b1{1., 2., 3., 4.};
     BoidState b2{0., 0., 0., 0.};
-    CHECK(((b1 - b2) == {1., 2., 3., 4.}));
+    CHECK(((b1 - b2) == b1));
   }
   SUBCASE("Check difference with two points ") {
     BoidState b1{1., 2., 3., 4.};
     BoidState b2{4., 3., 2., 1.};
-    CHECK(((b1 - b2) == {-3., -1., 1., 3.}));
+    CHECK((b1 - b2).x == -3.);
+    CHECK((b1 - b2).y == -1.);
+    CHECK((b1 - b2).v_x == 1.);
+    CHECK((b1 - b2).v_y == 3.);
   }
   SUBCASE("Check difference with negative numbers") {
     BoidState b1{1., 2., 3., 4.};
     BoidState b2{-1., -3., -2., -4.};
-    CHECK(((b1 - b2) == {2., 5., 5., 8.}));
+    CHECK((b1 - b2).x == 2.);
+    CHECK((b1 - b2).y == 5.);
+    CHECK((b1 - b2).v_x == 5.);
+    CHECK((b1 - b2).v_y == 8.);
   }
   SUBCASE("Check difference which is zero") {
     BoidState b1{1., 2., 3., 4.};
     BoidState b2{1., 2., 3., 4.};
-    CHECK(((b1 - b2) == {0., 0., 0., 0.}));
+    CHECK((b1 - b2).x == 0.);
+    CHECK((b1 - b2).y == 0.);
+    CHECK((b1 - b2).v_x == 0.);
+    CHECK((b1 - b2).v_y == 0.);
   }
-  SUBCASE("Check denominator == 0") {
+  /*SUBCASE("Check denominator == 0") {
     BoidState b1{2., 0., 5., 5.};
     BoidState b2{2., 3., 4., 0.};
     CHECK(operator/(b1, b2));
@@ -86,5 +106,30 @@ TEST_CASE("Testing operators") {
     BoidState b1{2., 0., 5., 5.};
     BoidState b2{2., 3., 4., 0.};
     CHECK((b1 * b2) == {4., 0., 20., 0});
+  }*/ //commentati perché non credo ci serviranno
+}
+
+TEST_CASE("Testing Cohesion rule") {
+  SUBCASE("testing with a vector of three") {
+    CohesionRule c1{3, 4};
+    BoidState b1{1., 2., 3., 4.};
+    BoidState b2{2., 3., 4., 5.};
+    BoidState b3{-1., -1., -1., -1.};
+    std::vector<BoidState> v1{b1, b2, b3};
+
+    CHECK(c1(v1).vel_x == -2.0);
+    CHECK(c1(v1).vel_y == -4.0);
+  }
+
+  SUBCASE("testing with a vector of four") {
+    CohesionRule c1{4, 1};
+    BoidState b1{1., 2., 3., 4.};
+    BoidState b2{2., 3., 4., 5.};
+    BoidState b3{-1., -1., -1., -1.};
+    BoidState b4{0., -1., 3., -2.};
+    std::vector<BoidState> v1{b1, b2, b3};
+
+    CHECK(c1(v1).vel_x == doctest::Approx(-0.67).epsilon(0.01));
+    CHECK(c1(v1).vel_y == doctest::Approx(-1.67).epsilon(0.01));
   }
 }
