a90f17b6626b5e7866e2fe5d49b43eacb803e593
Ilaria Antonellini <antonellinilaria@gmail.com>
2022-08-10 12:01:30 +0200

    Add declaration and definitions rules and boids

--- boids.hpp
+++ boids.hpp
@@ -1,9 +1,11 @@
 #ifndef BOIDS_HPP
 #define BOIDS_HPP
 
+#include <algorithm>
 #include <cassert>
 #include <cmath>
 #include <stdexcept>
+#include <vector>
 
 // manca il controllo degli errori su tutto
 
@@ -16,6 +18,10 @@ struct BoidState {
 
 BoidState operator==(BoidState const &b1, BoidState const &b2) {
   return {b1.x == b2.x, b1.y == b2.y, b1.v_x == b2.v_x, b1.v_y == b2.v_y};
+}
+
+BoidState operator!=(BoidState const &b1, BoidState const &b2) {
+  return {b1.x != b2.x, b1.y != b2.y, b1.v_x != b2.v_x, b1.v_y != b2.v_y};
 }
 
 BoidState operator+(BoidState const &b1, BoidState const &b2) {
@@ -48,6 +54,9 @@ struct VelocityComponents {
   double vel_y;
 };
 
+// idea : si potrebbero implementare le classi delle regole già con dei vettori
+// che restituiscono le regole -> dopo mando audio
+
 class SeparationRule {
   int const n_;
   double const separation_const_;
@@ -60,14 +69,80 @@ class SeparationRule {
 
   VelocityComponents operator()(BoidState const &b1,
                                 BoidState const &b2) const {
-    return;
+    return {};
   }
 };
 
-class AllignmentRule {};
+class AllignmentRule {
+  int const n_;
+  double const allignment_const_;
 
-class CohesionRule {};
+ public:
+  AllignmentRule(int const n, double const a) : n_{n}, allignment_const_{a} {}
 
-class Boids {};
+  VelocityComponents operator()(BoidState const &b1,
+                                BoidState const &b2) const {
+    return {};
+  }
+};
+
+class CohesionRule {
+  int const n_;
+  double const cohesion_const_;
+
+ public:
+  CohesionRule(int const n, double const c) : n_{n}, cohesion_const_{c} {}
+
+  VelocityComponents COM(int const n_, BoidState const &b1) { return {}; }
+
+  VelocityComponents operator()(BoidState const &b1,
+                                BoidState const &b2) const {
+    return {};
+  }
+};
+
+// dubbio : mettere la vaiabile n solo in boids e non  nelle classi delle regole
+// così sono tutte uguali ?
+
+class Boids {
+  int const n_;
+  double const distance_;
+  SeparationRule s_;
+  AllignmentRule a_;
+  CohesionRule c_;
+
+  std::vector<BoidState>
+      boids_;  // ho provato a mettere quella n sopra come definizione ma
+               // non funziona non ho capito perchè quindi boh -> faccio
+               // fatica a definire un numero fisso di entrate del vettore
+  BoidState solve(BoidState const &b1, VelocityComponents const &v1,
+                  VelocityComponents const &v2, VelocityComponents const &v3,
+                  double const delta_t) const {
+    return {};
+  }
+
+ public:
+  Boids(int const n, double const d, SeparationRule const &s,
+        AllignmentRule const &a, CohesionRule const &c)
+      : n_{n}, distance_{d}, s_{s}, a_{a}, c_{c} {}
+
+  bool empty() { return boids_.empty(); }
+
+  std::size_t size() const { return boids_.size(); }
+
+  void push_back(BoidState const &boid) {
+    // da mettere controllo che non ci siano boid con la stessa posizione e,
+    // fare loop fino a n_ perch avere vettore di quella dimensione e mettere
+    // assert su tutto / eccezioni -> comunque questo è l'invariante
+
+    boids_.push_back(boid);
+  }
+
+  void evolution(double const delta_t) {}
+
+  std::vector<BoidState> const &state() const {
+    return boids_;
+  }  // non capisco perchè dia errore qui
+};
 
 #endif
