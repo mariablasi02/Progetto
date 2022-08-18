25ced577dbf45fa7d216a68f749bbefa18702869
Ilaria Antonellini <antonellinilaria@gmail.com>
2022-08-18 11:06:51 +0200

    Add function simulate

--- boids.cpp
+++ boids.cpp
@@ -164,7 +164,7 @@ void Boids::evolution(double const delta_t) {
 
   assert(size(fishes) == size(boids_));
   boids_ = fishes;
-  assert(same_pos_check(boids_));
+  assert(same_pos_check(boids_)==true);
 }
 
 void Boids::setvector(std::vector<BoidState> const& b) {  // prova
