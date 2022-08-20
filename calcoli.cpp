8c6ea49d62ea9b6a92fb2cdd7040e2e89a0aa381
Ilaria Antonellini <antonellinilaria@gmail.com>
2022-08-20 15:33:46 +0200

    Fixed test with new singleboid function

--- calcoli.cpp
+++ calcoli.cpp
@@ -5,30 +5,26 @@
 #include "boids.hpp"
 
 int main() {
-  BoidState n1{0., 0., 0., 0.};
-  BoidState n2{0., 10., 0., 1.};
-  BoidState n3{0., 30., 0., 1.};
-  SeparationRule s{5, 3.};
-  AlignmentRule a{0.5};
-  CohesionRule c{3};
-  Boids bb{3, 60., s, a, c};
-  bb.push_back(n1);
-  bb.push_back(n2);
-  bb.push_back(n3);
-  bb.evolution(0.2);
-  std::cout << "x: " << bb.TotalBoids()[0].x << "y: " << bb.TotalBoids()[0].y
-            << " v_x: " << bb.TotalBoids()[0].v_x
-            << " v_y: " << bb.TotalBoids()[0].v_y << '\n';
-  std::cout << "x: " << bb.TotalBoids()[1].x << "y: " << bb.TotalBoids()[1].y
-            << " v_x: " << bb.TotalBoids()[1].v_x
-            << " v_y: " << bb.TotalBoids()[1].v_y << '\n';
-  std::cout << "x: " << bb.TotalBoids()[2].x << "y: " << bb.TotalBoids()[2].y
-            << " v_x: " << bb.TotalBoids()[2].v_x
-            << " v_y: " << bb.TotalBoids()[2].v_y << '\n';
-  std::cout<< norm(bb.TotalBoids()[0] , bb.TotalBoids()[1]) << '\n';
-  std::cout<< norm(bb.TotalBoids()[0], bb.TotalBoids()[2])<< '\n';
-  std::cout<< norm(bb.TotalBoids()[2], bb.TotalBoids()[1]) << '\n';
-  std::cout << velocity_norm(bb.TotalBoids()[0]) << '\n';
-  std::cout << velocity_norm(bb.TotalBoids()[1]) << '\n';
-  std::cout << velocity_norm(bb.TotalBoids()[2]) << '\n';
+  BoidState b1{3., 1., 0.3, 2.0};
+  BoidState b2{2., 1., 1.7, -0.7};
+  BoidState b3{1., 4., -0.5, 0.4};
+  SeparationRule s{0.2, 20};
+  AlignmentRule a{0.3};
+  CohesionRule c{1.3};
+  Boids boid{3, 40., s, a, c};
+  boid.push_back(b1);
+  boid.push_back(b2);
+  boid.push_back(b3);
+
+  /* std::cout << s(boid.TotalBoids(), b1).val_x << " "
+            << s(bb.TotalBoids(), b1).val_y << '\n';
+  std::cout << a(bb.TotalBoids(), b1).val_x << " "
+            << a(bb.TotalBoids(), b1).val_y << '\n';
+  std::cout << c(bb.TotalBoids(), b1).val_x << " "
+            << c(bb.TotalBoids(), b1).val_y << '\n'; */
+
+  std::cout << (boid.singleboid(boid.TotalBoids(), b2, 0.5)).v_x << '\n';
+  std::cout << (boid.singleboid(boid.TotalBoids(), b2, 0.5)).v_y << '\n';
+  std::cout << (boid.singleboid(boid.TotalBoids(), b1, 0.5)).v_x << '\n';
+  std::cout << (boid.singleboid(boid.TotalBoids(), b1, 0.5)).v_y << '\n';
 }
